9a8bc07babced78d0aec8ee4cc2ab28854f6a4ea
utkapodsousom <utkapodsousom@gmail.com>
2017-06-05 23:50:23 +0300

    remove old styling, add basic typography settings

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$(".menu-button").click(function(){
 		$(".menu-bar").toggleClass( "open" );
-	})
-})
+	});
 
-$(document).ready(function(){
 	$(".menu-bar").children().click(function(){
 		$(".menu-bar").children().clildren().removeClass("open");
-	})
+	});
+
+	
 })
